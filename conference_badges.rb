560f3da07ed790c83aea6ce6ed8fc0f7be01b025
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-16 04:39:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,5 +5,5 @@ def batch_badge_creator(array)
   array.collect{|name|"Hello, my name is #{name}."  }
 end
 def assign_rooms(speakers)
-  speakers.each_with_index{|speaker, index|"Hello, #{speaker} You'll be assigned to room #{index}!" }
+  speakers.each_with_index{|speaker, index|puts "Hello, #{speaker} You'll be assigned to room #{index}!" }
 end
