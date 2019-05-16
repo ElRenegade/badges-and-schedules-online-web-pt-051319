48401ea03c50a04ce1bdbdadd27d16402e717f34
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-16 04:27:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,5 +2,5 @@ def badge_maker(name)
    puts "Hello, my name is #{name}."
 end
 def batch_badge_creator(array)
-  array.collect{|name|"Hello my name is #{name}"  }
+  array.collect{|name|"Hello, my name is #{name}"  }
 end
